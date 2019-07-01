0e1c84ec85743bb7c67f28850389dfda81f16836
汪韬 <452663934@qq.com>
2019-07-01 17:28:05 +0800

    :wrench: 增加generator

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const devCmd = require('./src/cmd/dev');
 module.exports = function (api, options) {
     devCmd(api);
 };
+
+// module.exports = (api, options, rootOptions) => {
+//     api.extendPackage({
+//         devDependencies: {
+//             '@vusion/css-loader': '^0.4.0',
+//             '@vusion/doc-loader': '^0.10.1',
+//             '@vusion/md-vue-loader': '^1.0.1',
+//             '@vusion/vue-loader': '~14.2.1',
+//             'vue-multifile-loader': '~14.2.2',
+//             'base-css-image-loader': '^0.2.4',
+//         },
+//     });
+// };
