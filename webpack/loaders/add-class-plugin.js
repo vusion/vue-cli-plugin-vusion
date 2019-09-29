2bf0fc6d67cc52b5f7c361a348ad7a23ce719698
AlfredMou <moujintao@outlook.com>
2019-09-29 20:07:08 +0800

    :zap:  if class is not in components set after fixed :not(html)

--- webpack/loaders/add-class-plugin.js
+++ webpack/loaders/add-class-plugin.js
@@ -1,10 +1,18 @@
 const postcss = require('postcss');
 module.exports = postcss.plugin('add-class-plugin', ({ loaderContext, classList }) => (styles, result) => {
-    const classCheckList = classList.map((name) => `.${name}`);
     return Promise.resolve().then(() => {
+        const afterFixed = loaderContext.context && loaderContext.context.search('components') ? ':not(html)' : '[class]' ;
         styles.walkRules((rule) => {
-            if (classCheckList.includes(rule.selector))
-                rule.selector = rule.selector + '[class]';
+            const regx = new RegExp(`.([a-zA-Z0-9]*)(?![-_a-zA-Z0-9])`, 'g');
+            const reuslt = regx.exec(rule.selector);
+            if (reuslt && classList.includes(reuslt[1])) {
+                const name = reuslt[1];
+                rule.selector = rule.selector.replace(new RegExp(`.${name}(?![-_a-zA-Z0-9])`, 'g'), (n) => {
+                    return n + afterFixed;
+                });
+                console.log(rule.selector);
+            }
         });
     });
 });
+ 
