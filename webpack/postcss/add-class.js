320291b4c19f8dea15f3979efc50b0a7ffc7530f
AlfredMou <moujintao@outlook.com>
2019-09-30 10:09:43 +0800

    :zap: reduce the size of the change to not(html)

--- webpack/postcss/add-class.js
+++ webpack/postcss/add-class.js
@@ -1,7 +1,7 @@
 const postcss = require('postcss');
 module.exports = postcss.plugin('add-class-plugin', ({ loaderContext, classList }) => (styles, result) => {
     return Promise.resolve().then(() => {
-        const afterFixed = loaderContext.context && loaderContext.context.search('components') ? ':not(html)' : '[class]' ;
+        const afterFixed = loaderContext.context && loaderContext.context.includes('src/components') ? ':not(html)' : '[class]' ;
         styles.walkRules((rule) => {
             const regx = new RegExp(`.([a-zA-Z0-9]*)(?![-_a-zA-Z0-9])`, 'g');
             const reuslt = regx.exec(rule.selector);
