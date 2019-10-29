bcfabf21b17fc6abe03811a86f160236413af857
Rainfore <rainforest92@126.com>
2019-10-30 01:38:31 +0800

    :bug: Fix doc.base

--- webpack/loaders/module-class-priority-loader/fix-priority-plugin.js
+++ webpack/loaders/module-class-priority-loader/fix-priority-plugin.js
@@ -3,7 +3,7 @@ const { replaceSelector } = require('./utils');
 module.exports = postcss.plugin('fix-priority-plugin', ({ loaderContext, classList }) => (styles, result) => Promise.resolve().then(() => {
     // - 组件库加 :not(html)
     // - 业务中加 [class]
-    const suffix = loaderContext.context && loaderContext.context.includes('src/components') ? ':not(html)' : '[class]';
+    const suffix = loaderContext.context && (loaderContext.context.includes('src/components') || loaderContext.context.includes('src/global/components')) ? ':not(html)' : '[class]';
     styles.walkRules((rule) => {
         rule.selector = replaceSelector(rule.selector, classList, suffix);
     });
