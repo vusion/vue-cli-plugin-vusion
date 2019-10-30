acc794dcc59a602b7e3ae4524efdf2b63d6acad9
AlfredMou <moujintao@outlook.com>
2019-10-30 17:37:47 +0800

    :bug: Unprocessed form of $style[""]

---
+++ test/unit/getClassName.spec.js
@@ -0,0 +1,29 @@
+const { getClassName } = require('../../webpack/loaders/module-class-priority-loader/utils');
+const expect = require('chai').expect;
+const testValues = [{
+    value: '$style.test',
+    result: 'test',
+},{
+    value: '$style["test"]',
+    result: 'test',
+},{
+    value: "$style['test']",
+    result: 'test',
+}, {
+    value: '$style["test-1"]',
+    result: 'test-1',
+}, {
+    value: '$style["test-1$wewe12"]',
+    result: 'test-1$wewe12',
+}, {
+    value: '$style["112-tesrr"]',
+    result: '112-tesrr',
+}];
+
+describe('replace class in selector', () => {
+    it('selectName', () => {
+        for(const item of testValues) {
+            expect(getClassName(item.value)).to.equal(item.result);
+        }
+    });
+});
--- webpack/loaders/module-class-priority-loader/utils.js
+++ webpack/loaders/module-class-priority-loader/utils.js
@@ -3,11 +3,24 @@ const path = require('path');
 
 const DOM_LIST = ['html', 'base', 'head', 'link', 'meta', 'meta ', 'script', 'style', 'title', 'body', 'address', 'article', 'aside', 'footer', 'header', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hgroup', 'main', 'nav', 'section', 'blockquote', 'cite', 'dd', 'dl', 'dt', 'dir', 'div', 'figcaption', 'figure', 'hr', 'li', 'ol', 'ul', 'menu', 'p', 'pre', 'a', 'abbr', 'b', 'bdi', 'bdo', 'br', 'code', 'data', 'time', 'dfn', 'em', 'i', 'kbd', 'mark', 'q', 'rb', 'ruby', 'rp', 'rt', 'rtc', 's', 'del', 'ins', 'samp', 'small', 'span', 'strong', 'sub', 'sup', 'tt', 'u', 'var', 'wbr', 'area', 'audio', 'src', 'source', 'img', 'map', 'track', 'video', 'applet', 'embed', 'iframe', 'noembed', 'object', 'param', 'picture', 'canvas', 'noscript', 'caption', 'table', 'col', 'colgroup', 'tbody', 'td', 'tfoot', 'th', 'scope', 'headers', 'thead', 'tr', 'button', 'datalist', 'fieldset', 'form', 'input', 'label', 'legend', 'meter', 'optgroup', 'select', 'option', 'output', 'progress', 'textarea', 'details', 'dialog', 'menuitem', 'summary', 'content', 'slot', 'element', 'template', 'acronym', 'basefont', 'font', 'bgsound', 'big', 'blink', 'center', 'command', 'frame', 'frameset', 'image', 'isindex', 'keygen', 'listing', 'marquee', 'multicol', 'nextid', 'nobr', 'noframes', 'plaintext', 'shadow', 'spacer', 'strike', 'xmp', 'animate', 'animateMotion', 'animateTransform', 'circle', 'clipPath', 'color-profile', 'defs', 'desc', 'discard', 'ellipse', 'feBlend', 'feColorMatrix', 'feComponentTransfer', 'feComposite', 'feConvolveMatrix', 'feDiffuseLighting', 'feDisplacementMap', 'feDistantLight', 'feDropShadow', 'feFlood', 'feFuncA', 'feFuncB', 'feFuncG', 'feFuncR', 'feGaussianBlur', 'feImage', 'feMerge', 'feMergeNode', 'feMorphology', 'feOffset', 'fePointLight', 'feSpecularLighting', 'feSpotLight', 'feTile', 'feTurbulence', 'filter', 'foreignObject', 'g', 'hatch', 'hatchpath', 'line', 'linearGradient', 'marker', 'mask', 'mesh', 'meshgradient', 'meshpatch', 'meshrow', 'metadata', 'mpath', 'path', 'pattern', 'polygon', 'polyline', 'radialGradient', 'rect', 'set', 'solidcolor', 'stop', 'svg', 'switch', 'symbol', 'text', 'textPath', 'tspan', 'unknown', 'use', 'view'];
 
+function getClassName(styleStr) {
+    if (/^\$style\[['"](.*)['"]\]$/g.test(styleStr)) {
+        const classNameVarName = /^\$style\[['"](.*)['"]\]$/g.exec(styleStr);
+        if (classNameVarName)
+            return classNameVarName[1];
+    }else if (/^\$style./g.test(styleStr)) {
+        const classNameVarName = styleStr.split('.')[1];
+        if (classNameVarName)
+            return classNameVarName;
+    }
+    return undefined;
+};
+
 exports.replaceSelector = function replaceSelector(selector, classList, suffix = ':not(html)') {
     const classListStr = classList.join('|');
     return selector.replace(new RegExp(`.(${classListStr})(?![-_a-zA-Z0-9])`, 'g'), (n) => n + suffix);
 };
-
+exports.getClassName = getClassName;
 exports.getBindingClasses = function getBindingClasses(filePath) {
     return new Promise((res, rej) => {
         if (/\.vue$/g.test(filePath)) {
@@ -27,14 +40,11 @@ exports.getBindingClasses = function getBindingClasses(filePath) {
                 const classSet = new Set();
                 const content = String(data);
 
-                function pushClass(styleStr) {
-                    if (/^\$style./g.test(styleStr)) {
-                        const classNameVarName = styleStr.split('.')[1];
-                        if (classNameVarName)
-                            classSet.add(classNameVarName);
-                    }
+                const pushClass = (styleStr) => {
+                    const className = getClassName(styleStr);
+                    if (className)
+                        classSet.add(className);
                 }
-
                 content.replace(/<([^>\s]+)?\s.*:class="([^"]+)"/g, (m, $1, $2) => {
                     if (!DOM_LIST.includes($1) && $1 !== '!--') {
                         const className = $2;
@@ -49,7 +59,6 @@ exports.getBindingClasses = function getBindingClasses(filePath) {
                     }
                     return m;
                 });
-
                 res(Array.from(classSet));
             });
         } else {
