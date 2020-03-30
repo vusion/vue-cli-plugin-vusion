d3ee2232acd5fd19435eb40740bf6c93d2a1229e
Forrest <rainforest92@126.com>
2020-03-30 11:30:54 +0800

    :wrench: Use vusion-loader instead of vue-multifile-loader

--- service/chainDefault.js
+++ service/chainDefault.js
@@ -43,18 +43,10 @@ module.exports = function chainDefault(api, vueConfig, vusionConfig) {
         const resolveAlias = config.resolve.alias;
         Object.keys(alias).forEach((key) => resolveAlias.set(key, alias[key]));
 
-        config.module.rule('vue-multifile')
-            .test(/\.vue[\\/]index\.js$/)
-            .use('cache-loader')
-            .loader('cache-loader')
-            .options(config.module.rule('vue').use('cache-loader').get('options'))
-            .end()
-            .use('vue-loader')
-            .loader('vue-loader')
-            .options(config.module.rule('vue').use('vue-loader').get('options'))
-            .end()
-            .use('vue-multifile-loader')
-            .loader('vue-multifile-loader');
+        config.module.rule('vue')
+            .test(/\.vue([\\/]index\.js)?$/)
+            .use('vusion-loader')
+            .loader('vusion-loader');
 
         // config.module.rules.delete('postcss');
         // config.module.rules.delete('scss');
