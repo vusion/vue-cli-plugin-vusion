e031c9f3b652e381774b014ea9ea42da246679fc
Forrest <rainforest92@126.com>
2020-04-02 02:52:18 +0800

    :twisted_rightwards_arrows: Merge doc-loader <u-code-example>

--- scenes/doc/components/common/u-code-example.vue/index.js
+++ scenes/doc/components/common/u-code-example.vue/index.js
@@ -1,6 +1,7 @@
 import '../atom-one-light.css';
 import { formatValue } from '../u-data-view-node.vue/utils';
-import { utils } from 'cloud-ui.vusion';
+import { utils } from 'cloud-ui';
+import { getParameters } from 'codesandbox/lib/api/define';
 
 export default {
     name: 'u-code-example',
@@ -30,6 +31,7 @@ export default {
             githubLink: baseLink ? baseLink + '/' + this.filePath : false,
             anondemoData: {},
             logs: [],
+            codesandboxData: '',
         };
     },
     watch: {
@@ -74,6 +76,70 @@ export default {
         },
         clearLogs() {
             this.logs = [];
+        },
+        openCodesandbox() {
+            const content = this.$refs.code.innerText.trim();
+            this.codesandboxData = getParameters({
+                files: {
+                    'package.json': {
+                        content: {
+                            scripts: {
+                                serve: 'vue-cli-service serve',
+                                build: 'vue-cli-service build',
+                                lint: 'vue-cli-service lint',
+                            },
+                            dependencies: {
+                                'vusion-utils': 'latest',
+                                vue: '^2.6.10',
+                                'vue-template-compiler': '^2.6.10',
+                                'cloud-ui.vusion': '^0.6.14',
+                            },
+                            devDependencies: {
+                                '@vue/babel-preset-app': '^3.11.0',
+                                '@vue/cli-plugin-babel': '4.1.1',
+                                '@vue/cli-plugin-eslint': '4.1.1',
+                                '@vue/cli-service': '4.1.1',
+                                'vue-cli-plugin-vusion': '^0.9.4',
+                                'babel-eslint': '^10.0.3',
+                                eslint: '^6.7.2',
+                                'eslint-plugin-vue': '^6.0.1',
+                                'vue-template-compiler': '^2.6.11',
+                            },
+                        },
+                    },
+                    'src/App.vue': {
+                        content: content.trim().startsWith('<template') ? content : `<template><div>${content}</div></template>`,
+                    },
+                    'src/index.js': {
+                        content:
+`import Vue from "vue";
+import "cloud-ui.vusion/src/styles/base.css";
+import "cloud-ui.vusion/src/styles/theme.css";
+import "cloud-ui.vusion/dist-raw/index.css";
+import * as CloudUI from "cloud-ui.vusion/dist-raw/index.js";
+import Main from "./App.vue";
+import { install } from "vusion-utils";
+install(Vue, CloudUI);
+const Ctor = Vue.extend(Main);
+new Ctor().$mount("#app");`,
+                    },
+                    'index.html': {
+                        content:
+`<style>
+    #container {
+        padding: 20px; min-width: 200px; min-height: 200px;
+    }
+</style>
+<div id="container">
+    <div id="app">
+</div>
+</div>`,
+                    },
+                },
+            });
+            this.$nextTick(() => {
+                this.$refs.codesandbox.submit();
+            });
         },
     },
 };
