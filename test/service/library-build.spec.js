77c6e2ffbab494c738dec7979fcb60fc2bb96350
Forrest <rainforest92@126.com>
2020-04-07 10:44:48 +0800

    :green_heart: Fix CI error

--- test/service/library-build.spec.js
+++ test/service/library-build.spec.js
@@ -19,7 +19,7 @@ describe('vue-cli-service library-build', () => {
             url: '/demo.html',
         }, async ({ page, helpers }) => {
             expect((await helpers.getText('a[class^="u-button"]')).trim()).that.includes('Click me');
-            page.click('a[class^="u-button"]')
+            page.click('a[class^="u-button"]');
             await sleep(1000);
             expect(await helpers.getText('[class^="u-modal_title"]')).to.equal('提示');
             expect(await helpers.getText('[class^="u-modal_content"]')).to.equal('Hello World!');
@@ -32,5 +32,22 @@ describe('vue-cli-service library-build', () => {
         // expect(project.has('dist-raw/index.css')).to.be.true;
         expect(await project.read('dist-raw/index.js')).that.includes('const ');
         expect(await project.read('dist-raw/index.css')).that.includes('icon-font:url(');
+    });
+
+    it('cloud-ui:docs', () => {
+        project.exec('npm run build:docs');
+
+        // await serve({
+        //     root: path.join(process.cwd(), 'public'),
+        //     url: '/',
+        // }, async ({ page, helpers }) => {
+        //     expect((await helpers.getText('h1')).trim()).that.includes('UChip');
+        //     expect((await helpers.getText('[class^="u-chip"]')).trim()).to.equal('Chip');
+
+        //     page.click('a[title="API"]')
+        //     await sleep(1000);
+        //     expect(await helpers.hasElement('#events')).to.be.true;
+        //     expect(await helpers.hasElement('#before-remove')).to.be.true;
+        // });
     });
 });
