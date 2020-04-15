d59d48491ea817e75727518c8af8d3da005f86fa
汪韬 <452663934@qq.com>
2020-04-15 10:01:40 +0800

    :wrench: 增加对extend的处理

--- webpack/getVariablesPostcssPlugins.js
+++ webpack/getVariablesPostcssPlugins.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const postcssImportResolver = require('postcss-import-resolver');
 const properties = require('./postcss/postcss-custom-properties-reader');
+const postcssVusionExtendMark = require('./postcss/extend-mark');
+const postcssVusionExtendMerge = require('./postcss/extend-merge');
 const map2obj = ((map) => {
     const obj = {};
     map.forEach((value, key) => { obj[key] = value; });
@@ -8,13 +10,23 @@ const map2obj = ((map) => {
 });
 module.exports = function getVariablesPostcssPlugins(config) {
     const alias = map2obj(config.resolve.alias.store);
+    const postcssExtendMark = postcssVusionExtendMark({
+        resolve: postcssImportResolver({
+            extensions: ['.js'],
+            alias,
+        }),
+    });
     return [
+        postcssExtendMark,
         require('postcss-import')({
             resolve: postcssImportResolver({
                 alias,
             }),
+            skipDuplicates: false,
+            plugins: [postcssExtendMark],
         }),
         require('postcss-variables'),
+        postcssVusionExtendMerge,
         properties,
     ];
 };
