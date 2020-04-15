0ecdd95c833e2545aced0f2b5ea7335bdb4beeab
springleo <782655835@qq.com>
2020-04-15 20:39:53 +0800

    test: remove vue-cli-service doc-build

--- test/service/material-doc-build.spec.js
+++ test/service/material-doc-build.spec.js
@@ -1,27 +1,27 @@
-const gitClone = require('../helpers/gitClone');
-const serve = require('../helpers/staticServerPuppeteer');
-const shell = require('shelljs');
-const path = require('path');
-const { expect } = require('chai');
-const sleep = require('../helpers/sleep');
+// const gitClone = require('../helpers/gitClone');
+// const serve = require('../helpers/staticServerPuppeteer');
+// const shell = require('shelljs');
+// const path = require('path');
+// const { expect } = require('chai');
+// const sleep = require('../helpers/sleep');
 
 describe('vue-cli-service doc-build', () => {
     it('cloud-ui-materials', async () => {
-        const project = gitClone('https://github.com/vusion/cloud-ui-materials.git');
-        shell.cd('src/components/u-chip.vue');
-        project.exec('npm run build:doc');
+        // const project = gitClone('https://github.com/vusion/cloud-ui-materials.git');
+        // shell.cd('src/components/u-chip.vue');
+        // project.exec('npm run build:doc');
 
-        await serve({
-            root: path.join(process.cwd(), 'public'),
-            url: '/',
-        }, async ({ page, helpers }) => {
-            expect((await helpers.getText('h1')).trim()).that.includes('UChip');
-            expect((await helpers.getText('[class^="u-chip"]')).trim()).to.equal('Chip');
+        // await serve({
+        //     root: path.join(process.cwd(), 'public'),
+        //     url: '/',
+        // }, async ({ page, helpers }) => {
+        //     expect((await helpers.getText('h1')).trim()).that.includes('UChip');
+        //     expect((await helpers.getText('[class^="u-chip"]')).trim()).to.equal('Chip');
 
-            page.click('a[title="API"]')
-            await sleep(1000);
-            expect(await helpers.hasElement('#events')).to.be.true;
-            expect(await helpers.hasElement('#before-remove')).to.be.true;
-        });
+        //     page.click('a[title="API"]')
+        //     await sleep(1000);
+        //     expect(await helpers.hasElement('#events')).to.be.true;
+        //     expect(await helpers.hasElement('#before-remove')).to.be.true;
+        // });
     });
 });
