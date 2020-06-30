8ac2a3eb634fcb048972aa3ba6da0b8823038595
Forrest <rainforest92@126.com>
2020-06-30 13:28:26 +0800

    :ambulance: Clear require

--- service/registerDesigner.js
+++ service/registerDesigner.js
@@ -1,11 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-function readAndWriteFile(filePath, newRelativePath = '', handler) {
+function readAndWriteFileAndDeleteCache(filePath, newRelativePath = '', handler) {
     let content = fs.readFileSync(filePath, 'utf8');
     content = handler(content);
     const newPath = path.resolve(filePath, newRelativePath);
     fs.writeFileSync(newPath, content, 'utf8');
+
+    delete require.cache[newPath];
     return newPath;
 }
 
@@ -19,14 +21,14 @@ module.exports = function registerDesigner(api, vueConfig, vusionConfig, args) {
         const vueTemplateCompilerPath = require.resolve('vue-template-compiler');
         const vueTemplateCompilerBrowserPath = path.resolve(vueTemplateCompilerPath, '../browser.js');
         const vueTemplateCompilerBuildPath = path.resolve(vueTemplateCompilerPath, '../build.js');
-        readAndWriteFile(vueTemplateCompilerBrowserPath, '', (content) => content
+        readAndWriteFileAndDeleteCache(vueTemplateCompilerBrowserPath, '', (content) => content
             .replace(/(ast = parse\(template\.trim\(\), options\);)\s+(if|optimize)/g, `$1
                     (options.plugins || []).forEach((plugin) => plugin(ast, options, exports));
                 $2`)
             .replace('exports.compile = compile;\n  exports.compileToFunctions', 'exports.compile = compile;\n  exports.generate = generate;\n  exports.compileToFunctions')
             .replace('exports.ssrCompile = compile$1;\n  exports.ssrCompileToFunctions', 'exports.ssrCompile = compile$1;\n  exports.ssrGenerate = generate$1;\n  exports.ssrCompileToFunctions'));
 
-        readAndWriteFile(vueTemplateCompilerBuildPath, '', (content) => content
+        readAndWriteFileAndDeleteCache(vueTemplateCompilerBuildPath, '', (content) => content
             .replace(/(ast = parse\(template\.trim\(\), options\);)\s+(if|optimize)/g, `$1
                 (options.plugins || []).forEach((plugin) => plugin(ast, options, exports));\n$2`)
             .replace('exports.compile = compile;\nexports.compileToFunctions', 'exports.compile = compile;\nexports.generate = generate;\nexports.compileToFunctions')
@@ -34,13 +36,13 @@ module.exports = function registerDesigner(api, vueConfig, vusionConfig, args) {
 
         const vueLoaderPath = require.resolve('vue-loader');
         // const templateLoaderPath = path.resolve(vueLoaderPath, '../loaders/templateLoader.js');
-        // readAndWriteFile(templateLoaderPath, '', (content) => content
+        // readAndWriteFileAndDeleteCache(templateLoaderPath, '', (content) => content
         //     .replace('scopeId: query.scoped ? `data-v-${id}` : null,\n    comments', 'scopeId: query.scoped ? `data-v-${id}` : null,\n'
         //     + `  filename: require('path').relative(loaderContext.rootContext, this.resourcePath),`
         //     + '\n    comments'));
         //     .replace('scopeId: query.scoped ? `data-v-${id}` : null,', 'scopeId: `data-v-${id}`,\n  filename: this.resourcePath,'));
 
-        readAndWriteFile(vueLoaderPath, '', (content) => content
+        readAndWriteFileAndDeleteCache(vueLoaderPath, '', (content) => content
             .replace('const hasScoped = descriptor.styles.some(s => s.scoped)', 'const hasScoped = true'));
     }
 
