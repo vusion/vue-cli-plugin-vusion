6d65808d2356c116e194fc96b3399466153e124e
Forrest <rainforest92@126.com>
2020-07-02 21:57:25 +0800

    :bug: Only use pre way

--- service/registerDesigner.js
+++ service/registerDesigner.js
@@ -1,13 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-
-function readAndWriteFile(filePath, newRelativePath = '', handler) {
-    let content = fs.readFileSync(filePath, 'utf8');
-    content = handler(content);
-    const newPath = path.resolve(filePath, newRelativePath);
-    fs.writeFileSync(newPath, content, 'utf8');
-    return newPath;
-}
+const predesigner = require('./predesigner');
 
 module.exports = function registerDesigner(api, vueConfig, vusionConfig, args) {
     if (args._[0] === 'designer') {
@@ -16,36 +7,9 @@ module.exports = function registerDesigner(api, vueConfig, vusionConfig, args) {
         vueConfig.devServer.open = false;
 
         // 前置暴力替换，防止重启的问题
-        if (args.pre) {
-            const vueTemplateCompilerPath = require.resolve('vue-template-compiler');
-            const vueTemplateCompilerBrowserPath = path.resolve(vueTemplateCompilerPath, '../browser.js');
-            const vueTemplateCompilerBuildPath = path.resolve(vueTemplateCompilerPath, '../build.js');
-            readAndWriteFile(vueTemplateCompilerBrowserPath, '', (content) => content
-                .replace(/(ast = parse\(template\.trim\(\), options\);)\s+(if|optimize)/g, `$1
-                        (options.plugins || []).forEach((plugin) => plugin(ast, options, exports));
-                    $2`)
-                .replace('exports.compile = compile;\n  exports.compileToFunctions', 'exports.compile = compile;\n  exports.generate = generate;\n  exports.compileToFunctions')
-                .replace('exports.ssrCompile = compile$1;\n  exports.ssrCompileToFunctions', 'exports.ssrCompile = compile$1;\n  exports.ssrGenerate = generate$1;\n  exports.ssrCompileToFunctions'));
-
-            readAndWriteFile(vueTemplateCompilerBuildPath, '', (content) => content
-                .replace(/(ast = parse\(template\.trim\(\), options\);)\s+(if|optimize)/g, `$1
-                    (options.plugins || []).forEach((plugin) => plugin(ast, options, exports));\n$2`)
-                .replace('exports.compile = compile;\nexports.compileToFunctions', 'exports.compile = compile;\nexports.generate = generate;\nexports.compileToFunctions')
-                .replace('exports.ssrCompile = compile$1;\nexports.ssrCompileToFunctions', 'exports.ssrCompile = compile$1;\nexports.ssrGenerate = generate$1;\nexports.ssrCompileToFunctions'));
-
-            const vueLoaderPath = require.resolve('vue-loader');
-            // const templateLoaderPath = path.resolve(vueLoaderPath, '../loaders/templateLoader.js');
-            // readAndWriteFile(templateLoaderPath, '', (content) => content
-            //     .replace('scopeId: query.scoped ? `data-v-${id}` : null,\n    comments', 'scopeId: query.scoped ? `data-v-${id}` : null,\n'
-            //     + `  filename: require('path').relative(loaderContext.rootContext, this.resourcePath),`
-            //     + '\n    comments'));
-            //     .replace('scopeId: query.scoped ? `data-v-${id}` : null,', 'scopeId: `data-v-${id}`,\n  filename: this.resourcePath,'));
-
-            readAndWriteFile(vueLoaderPath, '', (content) => content
-                .replace('const hasScoped = descriptor.styles.some(s => s.scoped)', 'const hasScoped = true'));
-
-            process.exit();
-        }
+        predesigner();
+        // npx vue-cli-service designer --pre
+        args.pre && process.exit();
     }
 
     const serveCommand = api.service.commands.serve;
