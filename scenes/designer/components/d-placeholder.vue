5239086ba1da1ff4155da7517b08f4634cf150e7
zxr <zxrxiaorong@gmail.com>
2020-10-09 21:47:40 +0800

    :sparkles: 表达式双击可编辑

--- scenes/designer/components/d-placeholder.vue
+++ scenes/designer/components/d-placeholder.vue
@@ -1,5 +1,12 @@
 <template>
-<span :vusion-node-path="nodePath" :vusion-parent-node-path="parentNodePath"><slot>{{ text }}</slot></span>
+<span
+    :class="$style.root"
+    :vusion-node-path="nodePath"
+    :vusion-parent-node-path="parentNodePath"
+    @dblclick="onDblclick">
+    <!-- <slot>{{ text }}</slot> -->
+    {{ name }}
+</span>
 </template>
 
 <script>
@@ -7,8 +14,30 @@ export default {
     name: 'd-placeholder',
     props: {
         text: String,
+        name: String,
         nodePath: String,
         parentNodePath: String,
     },
+    methods: {
+        onDblclick() {
+            this.send({
+                command: 'editExpression',
+                nodePath: this.nodePath,
+                value: this.name,
+            });
+        },
+        send(data) {
+            return this.$root.$emit('d-slot:send', data);
+        },
+    },
 };
 </script>
+
+<style module>
+.root{
+    background: #ddd;
+    display: inline-block;
+    min-width:10px;
+    cursor: pointer !important;
+}
+</style>
