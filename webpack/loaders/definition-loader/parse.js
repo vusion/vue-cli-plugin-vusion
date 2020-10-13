bedc98eeaf7bdf2fb4355388f49479e2dc32c4fc
zhangxiaoyu06 <zhangxiaoyu06@corp.netease.com>
2020-10-13 21:32:49 +0800

    feat: support buildInFunction in buildInFunction

--- webpack/loaders/definition-loader/parse.js
+++ webpack/loaders/definition-loader/parse.js
@@ -50,6 +50,7 @@ module.exports = function (source) {
         next && Array.isArray(next) && next.forEach((child, index) => walk(child, func, node, index));
         next = node.arguments;
         next && Array.isArray(next) && next.forEach((child, index) => walk(child, func, node, index));
+        next && Array.isArray(next) && next.forEach((child, index) => walk(child, func, node, index));
 
         node.left && walk(node.left, func, node);
         node.right && walk(node.right, func, node);
@@ -91,7 +92,7 @@ module.exports = function (source) {
             return checkThis(node.object);
         }
     }
-    function safeGenerate(node) {
+    function safeGenerate(node, func) {
         if (!node)
             return undefined;
         checkThis(node);
@@ -108,7 +109,7 @@ module.exports = function (source) {
         if (logic.definition.returns[0])
             returnObj = logic.definition.returns[0];
 
-        walk(logic.definition, (node, parent, index) => {
+        const parseFunc = (node, parent, index) => {
             if (node.type === 'Start' || node.type === 'Comment') {
                 node.type = 'Noop';
             } else if (node.type === 'End') {
@@ -129,23 +130,27 @@ module.exports = function (source) {
                 // 参数
                 const getParams = () =>
                     // 函数参数有固定顺序的
-                    node.params
+                    (node.params || [])
                         .map((param) => {
+                            // 参数如果是内置对象，继续调用 walk 转化，如果是其他类型再针对性的转化，比如 CallInterface 之类的，也可以走 walk
+                            if (param.value.type === 'BuildInFunction') {
+                                return walk(param.value, parseFunc);
+                            }
+
                             const value = safeGenerate(param.value);
                             if (!value) {
-                                return null;
+                                return 'null';
                             } else {
                                 return `${value}`;
                             }
                         });
-
                 // 调用表达式
-                Object.assign(node, babel.parse(`this.$utils['${node.name}'](${getParams().join(',\n')})`, { filename: 'file.js' }).program.body[0].expression);
+                Object.assign(node, babel.parse(`this.$utils['${node.interfaceKey}'](${(getParams() || []).join(',\n')})`, { filename: 'file.js' }).program.body[0].expression);
             } else if (node.type === 'CallInterface') {
                 const key = node.interfaceKey;
                 const arr = key.split('/');
                 const getParams = (key) => {
-                    // 过滤掉 null 掉 param
+                    // 过滤掉 null 的 param
                     const nodeParams = (node.params || []).filter((param) => (param !== null && param !== 'null' && param !== ''));
                     if (key === 'body') {
                         const body = (nodeParams || [])
@@ -366,7 +371,9 @@ module.exports = function (source) {
                 throwStatement.argument.arguments = node.arguments;
                 Object.assign(node, throwStatement);
             }
-        });
+        };
+
+        walk(logic.definition, parseFunc);
 
         console.info('JSON generate:', JSON.stringify(logic.definition.body));
 
